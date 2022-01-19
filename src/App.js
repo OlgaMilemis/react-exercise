b307a54dbf83d46a6d8f0370a1c9a48a701325c2
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2022-01-19 16:33:57 +0100

    Created structure and removed unnecessary files and logos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Weather />
+      </header>
+      <footer>
+        {" "}
+        React exercise coded by caro and
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="gitHubLink"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {""} opensourced
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
