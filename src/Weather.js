2eb95142af5081f2f7e83d0c3697e2103e6619f0
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2022-01-19 17:55:20 +0100

    added Bootstrap, fonts and html

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,43 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
     <div className="Weather">
-      <h1>Hello from Weather</h1>
+      <form className="row mt-5">
+        <div className="col-9">
+          <input
+            type="search"
+            placeholder="Enter a city"
+            autoFocus="on"
+            className="form-control"
+          />
+        </div>
+        <div className="col-3">
+          <input type="Submit" value="Search" className="btn btn-success" />
+        </div>
+      </form>
+      <h1 className="mt-5">Berlin</h1>
+      <ul>
+        <li>Wednesday 16:52</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row mt-5">
+        <div className="col-6">
+          <img
+            className="icon"
+            src="https://www.accuweather.com/images/weathericons/07.svg"
+          />
+          <span className="temperature">2</span>
+          <span className="units">°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: 70%</li>
+            <li>Windsepeed: 5 km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
